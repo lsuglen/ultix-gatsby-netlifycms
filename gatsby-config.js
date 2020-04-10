fe7c5af6f02c4d2d85945096c432c07689e16127
lsuglen <lsuglen@users.noreply.github.com>
2020-04-09 21:56:16 -0500

    test change ultix logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Ultix Software, Inc.',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Ultix Software delivers state-of-the-art website design and custom software development solutions on Microsoft .NET, JAMStack and AgilityCMS.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
