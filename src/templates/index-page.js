ba3f443ec311a5e285ca694d7540dd3fd9e59ee2
lsuglen <lsuglen@users.noreply.github.com>
2020-04-14 23:28:08 -0500

    updated logo and index content

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,11 +40,12 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(34, 59, 30) 0.5rem 0px 0px, rgb(34, 59, 30) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(34, 59, 30)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: "center"
           }}
         >
           {title}
@@ -53,11 +54,12 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              'rgb(34, 59, 30) 0.5rem 0px 0px, rgb(34, 59, 30) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(34, 59, 30)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
+            textAlign: "center"
           }}
         >
           {subheading}
